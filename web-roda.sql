c18bc1c6a9790c1abd12e66772b26ea6eda7bab0
fikrihaikal20 <78941044+fikrihaikal20@users.noreply.github.com>
2021-07-12 18:07:08 +0700

    Update web-roda.sql

--- web-roda.sql
+++ web-roda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2021 at 04:35 PM
+-- Generation Time: Jul 12, 2021 at 01:05 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -49,10 +49,18 @@ INSERT INTO `admin` (`id`, `nama`, `email`, `password`) VALUES
 
 CREATE TABLE `anggota` (
   `id` int(11) NOT NULL,
-  `nama` int(11) NOT NULL,
-  `email` int(11) NOT NULL,
-  `password` int(11) NOT NULL
+  `nama` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `kelas` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `anggota`
+--
+
+INSERT INTO `anggota` (`id`, `nama`, `email`, `password`, `kelas`) VALUES
+(4, 'fikri haikal', 'fikri.haikal234@gmail.com', 'haikal', 'XI MIPA 7');
 
 -- --------------------------------------------------------
 
@@ -67,6 +75,35 @@ CREATE TABLE `pendaftar` (
   `password` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `pendaftar`
+--
+
+INSERT INTO `pendaftar` (`id`, `nama`, `email`, `password`) VALUES
+(3, 'fikri haikal', 'fikri.haikal234@gmail.com', '$2y$10$YkoegO8hLic.SymnPCliveg5AnoO2m6ybo1QLG5/PdzGkSZ6Pcxmu'),
+(4, 'fikri haikal', 'fikri.haikalanhali@gmail.com', '$2y$10$ZiE19G.Wtbl8PPf75iJEbOft.nl7377aVh7t3WvnV4gIIDUVMe5X2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `programmers`
+--
+
+CREATE TABLE `programmers` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `kelas` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `programmers`
+--
+
+INSERT INTO `programmers` (`id`, `nama`, `email`, `password`, `kelas`) VALUES
+(1, 'radit', 'radit@gmail.com', 'radit', 'XI MIPA 4');
+
 -- --------------------------------------------------------
 
 --
@@ -77,8 +114,86 @@ CREATE TABLE `robotics` (
   `id` int(11) NOT NULL,
   `nama` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
-  `password` varchar(128) NOT NULL
+  `password` varchar(128) NOT NULL,
+  `kelas` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `robotics`
+--
+
+INSERT INTO `robotics` (`id`, `nama`, `email`, `password`, `kelas`) VALUES
+(0, 'gentala', 'gentala@gmail.com', 'gentala', 'XI MIPA 8'),
+(1, 'Putra adam', 'adam@gmail.com', 'adam', 'XI MIPA 4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tugas`
+--
+
+CREATE TABLE `tugas` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(128) NOT NULL,
+  `deskripsi` varchar(128) NOT NULL,
+  `file` varchar(128) NOT NULL,
+  `divisi` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tugas`
+--
+
+INSERT INTO `tugas` (`id`, `judul`, `deskripsi`, `file`, `divisi`) VALUES
+(1, '', 'asdad', 'pat_prakarya.pdf', 'webdev'),
+(2, '', 'adad', 'jurnal_ramadhan.pdf', 'programmers'),
+(3, '', 'adad', 'jurnal_ramadhan1.pdf', 'programmers'),
+(4, '', 'adad', 'optic.pdf', 'robotics'),
+(5, '', 'asd', 'optic1.pdf', 'webdev'),
+(6, '', 'adad', 'TDS_SUKOREZ_SU-100_EN.pdf', 'webdev');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_access_menu`
+--
+
+CREATE TABLE `user_access_menu` (
+  `id` int(11) NOT NULL,
+  `role_id` int(2) NOT NULL,
+  `menu_id` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 2, 2),
+(5, 2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`) VALUES
+(1, 'admin'),
+(2, 'tugas'),
+(3, 'user');
 
 -- --------------------------------------------------------
 
@@ -88,10 +203,18 @@ CREATE TABLE `robotics` (
 
 CREATE TABLE `webdev` (
   `id` int(11) NOT NULL,
-  `nama` int(128) NOT NULL,
-  `email` int(128) NOT NULL,
-  `password` int(128) NOT NULL
+  `nama` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `kelas` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `webdev`
+--
+
+INSERT INTO `webdev` (`id`, `nama`, `email`, `password`, `kelas`) VALUES
+(1, 'haikal', 'haikal@gmail.com', 'haikal', 'XI MIPA 7');
 
 --
 -- Indexes for dumped tables
@@ -116,9 +239,27 @@ ALTER TABLE `pendaftar`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `programmers`
+--
+ALTER TABLE `programmers`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `robotics`
 --
 ALTER TABLE `robotics`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tugas`
+--
+ALTER TABLE `tugas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -141,25 +282,43 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `anggota`
 --
 ALTER TABLE `anggota`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `pendaftar`
 --
 ALTER TABLE `pendaftar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `programmers`
+--
+ALTER TABLE `programmers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `robotics`
 --
 ALTER TABLE `robotics`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tugas`
+--
+ALTER TABLE `tugas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `webdev`
 --
 ALTER TABLE `webdev`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
